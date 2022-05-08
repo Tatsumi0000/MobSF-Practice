d585fef272fb6358ed46229bcd9bd144d9ae3fb1
Tatsumi0000 <datsunote0211@gmail.com>
2022-05-08 22:10:14 +0900

    Androidアプリのフォルダに差分があったら動くようにした

---
+++ MobSF-iOS/Shared/MobSF_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobSF_iOSApp.swift
+//  Shared
+//
+//  Created by tatsumi.nishida on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct MobSF_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
